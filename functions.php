6493715246ce1077885a1bc31ecf59884c48825b
Wajid11 <aawajid987@gmail.com>
2021-10-10 17:15:39 +0500

    equefile

--- functions.php
+++ functions.php
@@ -2,4 +2,16 @@
 /*
 this tempalte is used for functions
 */
+
+
+function mytheme_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+//    wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css', array(), '1.1', 'all');
+
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+
+}
+add_action( 'wp_enqueue_scripts', 'mytheme_scripts' );
+
 ?>
