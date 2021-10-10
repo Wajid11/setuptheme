8d213a662fc560320a186871e9d3da19aceaaff8
Wajid11 <aawajid987@gmail.com>
2021-10-10 17:31:50 +0500

    equefile js

--- functions.php
+++ functions.php
@@ -9,7 +9,8 @@ function mytheme_scripts() {
 
 //    wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css', array(), '1.1', 'all');
 
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+//    wp_enqueue_script( 'external', get_template_directory_uri() . '/js/external.js', array ( 'jquery' ), 1.1, true);
+    wp_enqueue_script( 'external', get_template_directory_uri() . '/assest/js/myjs.js',true);
 
 }
 add_action( 'wp_enqueue_scripts', 'mytheme_scripts' );
